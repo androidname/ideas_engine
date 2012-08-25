85dd20d2c1db786664546bbe464f781c377c2ea0
Vladimir <vladimir.programmer@gmail.com>
2012-08-25 10:09:14 +0300

    first commit

---
+++ application/classes/controller/default.php
@@ -0,0 +1,18 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Default extends Controller_Template {
+
+        public $template = "layout/default";
+        
+        public function before() {
+            parent::before();
+            
+        }
+
+
+        public function action_index()
+	{
+            
+        }
+
+} // End Welcome
---
+++ application/logs/2012/08/25.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2012-08-25 01:47:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2012-08-25 01:47:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\VirtualServer\domains\idealab_engine\index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-08-25 01:54:33 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2012-08-25 01:54:33 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\VirtualServer\domains\idealab_engine\index.php(109): Kohana_Request->execute()
+#1 {main}
